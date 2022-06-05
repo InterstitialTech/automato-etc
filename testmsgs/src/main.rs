7955f26463e85ac1ee5ee87a22ab4eec5b047190
Ben Burdette <bburdette@protonmail.com>
2022-06-05 17:05:55 -0600

    strip out matomsg stuff from testmsgs

--- testmsgs/src/main.rs
+++ testmsgs/src/main.rs
@@ -19,16 +19,6 @@ use std::time::Duration;
 use serial::{BaudRate, CharSize, FlowControl, Parity, PortSettings, SerialPort, StopBits};
 
 fn main() {
-    // unsafe {
-    //     match readMessageFiles() {
-    //         Ok(()) => (),
-    //         Err(e) => {
-    //             println!("error: {:?}", e)
-    //         }
-    //     }
-    // }
-    writeMessageFiles();
-
     match err_main() {
         Ok(()) => (),
         Err(e) => {
@@ -38,334 +28,43 @@ fn main() {
 }
 
 fn err_main() -> Result<(), Box<dyn Error>> {
-    let matches = clap::App::new("matomsg")
+    let matches = clap::App::new("testmsgs")
         .version("1.0")
         .author("Automato Enterprises")
-        .about("cli for testing automato messsages over serial.")
+        .about("cli for testing automato messsages to/from files.")
         .arg(
-            Arg::with_name("port")
-                .short('p')
-                .long("port")
-                .value_name("FILE")
-                .help("serial port")
-                .default_value("/dev/ttyUSB0")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("baud")
-                .short('b')
-                .long("baud")
-                .value_name("NUMBER")
-                .help(
-                    "baud rate: 110, 300, 600, 1200, 2400, 4800, 9600, 19200, 38400, 57600, 115200",
-                )
-                .default_value("115200")
-                .takes_value(true),
-        )
-        .arg(
-            Arg::with_name("address")
-                .short('a')
-                .long("address")
-                .value_name("0-255")
-                .help("lora network address of an automato")
+            Arg::with_name("dir")
+                .short('d')
+                .long("directory")
+                .value_name("<director>")
+                .help("directory to write files to, or read files from.")
                 .required(true)
                 .takes_value(true),
         )
         .subcommand_required(true)
-        .subcommand(
-            Command::new("writepin")
-                .about("write 0 or 1 to pin")
-                .arg(Arg::with_name("pin").value_name("PIN").takes_value(true))
-                .arg(
-                    Arg::with_name("value")
-                        .value_name("1 or 0")
-                        .takes_value(true),
-                ),
-        )
-        .subcommand(
-            Command::new("pinmode")
-                .about("write 0 or 1 to pin")
-                .arg(Arg::with_name("pin").value_name("PIN").takes_value(true))
-                .arg(
-                    Arg::with_name("value")
-                        .value_name("1 or 0")
-                        .takes_value(true),
-                ),
-        )
-        .subcommand(
-            Command::new("readpin")
-                .about("query pin")
-                .arg(Arg::with_name("pin").value_name("PIN").takes_value(true)),
-        )
-        .subcommand(
-            Command::new("readanalog")
-                .about("query pin")
-                .arg(Arg::with_name("pin").value_name("PIN").takes_value(true)),
-        )
-        .subcommand(Command::new("readinfo").about("read automato general info"))
-        .subcommand(Command::new("readhumidity").about("read automato humidity"))
-        .subcommand(Command::new("readtemperature").about("read automato temperature"))
-        .subcommand(
-            Command::new("writemem")
-                .about("write hex data to automato memory")
-                .arg(
-                    Arg::with_name("address")
-                        .value_name("NUMBER")
-                        .takes_value(true),
-                )
-                .arg(
-                    Arg::with_name("value")
-                        .value_name("hex string")
-                        .takes_value(true),
-                ),
-        )
-        .subcommand(
-            Command::new("readmem")
-                .about("read hex data from automato memory")
-                .arg(
-                    Arg::with_name("address")
-                        .value_name("NUMBER")
-                        .takes_value(true),
-                )
-                .arg(
-                    Arg::with_name("length")
-                        .value_name("NUMBER")
-                        .takes_value(true),
-                ),
-        )
-        .subcommand(
-            Command::new("readfield")
-                .about("read field info from automato memory map")
-                .arg(
-                    Arg::with_name("index")
-                        .value_name("NUMBER")
-                        .takes_value(true),
-                ),
-        )
+        .subcommand(Command::new("write").about("write files"))
+        .subcommand(Command::new("read").about("read files"))
         .get_matches();
-
-    let (port, baud, automatoaddr) = match (
-        matches.value_of("port"),
-        matches.value_of("baud"),
-        matches.value_of("address"),
-    ) {
-        (Some(port), Some(baudstr), Some(addrstr)) => {
-            let baud = BaudRate::from_speed(baudstr.parse::<usize>()?);
-            let addr = addrstr.parse::<u8>()?;
-            (port, baud, addr)
-        }
-        _ => bail!("arg failure"),
-    };
-
-    let mut mb = Msgbuf {
-        buf: [0; am::RH_RF95_MAX_MESSAGE_LEN],
-    };
-
-    let mut retmsg = mb.clone();
 
     // set up the outgoing message.
     match matches.subcommand() {
-        Some(("writepin", sub_matches)) => {
-            let (pin, val) = match (sub_matches.value_of("pin"), sub_matches.value_of("value")) {
-                (Some(pinstr), Some(valstr)) => (pinstr.parse::<u8>()?, valstr.parse::<u8>()?),
-                _ => bail!("arg failure"),
-            };
-
-            unsafe { am::setup_writepin(&mut mb.payload, pin, val) };
+        Some(("write", sub_matches)) => {
+            writeMessageFiles();
         }
-        Some(("pinmode", sub_matches)) => {
-            let (pin, val) = match (sub_matches.value_of("pin"), sub_matches.value_of("value")) {
-                (Some(pinstr), Some(valstr)) => (pinstr.parse::<u8>()?, valstr.parse::<u8>()?),
-                _ => bail!("arg failure"),
-            };
-
-            unsafe { am::setup_pinmode(&mut mb.payload, pin, val) };
-        }
-        Some(("readpin", sub_matches)) => {
-            let pin = match sub_matches.value_of("pin") {
-                Some(pinstr) => pinstr.parse::<u8>()?,
-                _ => bail!("arg failure"),
-            };
-            unsafe { am::setup_readpin(&mut mb.payload, pin) };
-        }
-        Some(("readanalog", sub_matches)) => {
-            let pin = match sub_matches.value_of("pin") {
-                Some(pinstr) => pinstr.parse::<u8>()?,
-                _ => bail!("arg failure"),
-            };
-            unsafe { am::setup_readanalog(&mut mb.payload, pin) };
-        }
-        Some(("readinfo", sub_matches)) => {
-            unsafe { am::setup_readinfo(&mut mb.payload) };
-        }
-        Some(("readhumidity", sub_matches)) => {
-            unsafe { am::setup_readhumidity(&mut mb.payload) };
-        }
-        Some(("readtemperature", sub_matches)) => {
-            unsafe { am::setup_readtemperature(&mut mb.payload) };
-        }
-        Some(("writemem", sub_matches)) => {
-            let (addr, val) = match (
-                sub_matches.value_of("address"),
-                sub_matches.value_of("value"),
-            ) {
-                (Some(addrstr), Some(valstr)) => (addrstr.parse::<u16>()?, hex::decode(valstr)?),
-                _ => bail!("arg failure"),
-            };
-
-            unsafe { am::setup_writemem(&mut mb.payload, addr, val.as_slice()) };
-        }
-        Some(("readmem", sub_matches)) => {
-            let (addr, len) = match (
-                sub_matches.value_of("address"),
-                sub_matches.value_of("length"),
-            ) {
-                (Some(addrstr), Some(lenstr)) => (addrstr.parse::<u16>()?, lenstr.parse::<u8>()?),
-                _ => bail!("arg failure"),
-            };
-            unsafe { am::setup_readmem(&mut mb.payload, addr, len) };
-        }
-        Some(("readfield", sub_matches)) => {
-            let index = match sub_matches.value_of("index") {
-                Some(istr) => istr.parse::<u16>()?,
-                _ => bail!("arg failure"),
-            };
-            unsafe { am::setup_readfield(&mut mb.payload, index) };
-        }
+        Some(("read", sub_matches)) => unsafe {
+            match readMessageFiles() {
+                Ok(()) => (),
+                Err(e) => {
+                    println!("error: {:?}", e)
+                }
+            }
+        },
         meh => {
             bail!("unhandled command! {:?}", meh)
         }
     }
 
-    let mut port = serial::open(port)?;
-
-    let ps = PortSettings {
-        baud_rate: baud,
-        char_size: CharSize::Bits8,
-        parity: Parity::ParityNone,
-        stop_bits: StopBits::Stop1,
-        flow_control: FlowControl::FlowNone,
-    };
-    port.configure(&ps)?;
-
-    let readReply = true;
-    unsafe {
-        writeMessage(&mut port, &mb, automatoaddr)?;
-
-        let mut fromid: u8 = 0;
-        sleep(Duration::from_millis(420));
-
-        if readReply {
-            match readMessage(&mut port, &mut retmsg, &mut fromid) {
-                Ok(true) => {
-                    println!("reply from: {}", fromid);
-                    // for i in 0..retmsg.buf.len() {
-                    //     let c = retmsg.buf[i];
-                    //     println!("{} - {}", c, c as char);
-                    // }
-                    am::print_payload(&retmsg.payload);
-                }
-                Ok(false) => {
-                    println!("here");
-                }
-                Err(e) => {
-                    println!("error: {:?}", e);
-                }
-            }
-        } else {
-            let mut monobuf = [0; 1];
-            let mut count = 0;
-            while port.read_exact(&mut monobuf).is_ok() {
-                // just print the chars we read.  good for debug from Serial.print() on the automato.
-                // print!("{}", monobuf[0] as char);
-
-                // println!("{} '{}'", monobuf[0] as u8, monobuf[0] as char);
-
-                // print the index, number, and char
-                println!("{} - {} - {}", count, monobuf[0] as u8, monobuf[0] as char);
-                count = count + 1;
-            }
-            // let mut buf = String::new();
-            // let mut monobuf = [0; 1];
-            // port.read_exact(&mut monobuf)?;
-            // buf.push(monobuf[0] as char);
-            // if monobuf[0] as char == '\n' {
-            //     println!("msg: {}", buf);
-            // }
-        }
-    }
     Ok(())
-}
-
-fn old_main() -> Result<(), Box<dyn Error>> {
-    let mut port = serial::open("/dev/ttyUSB0")?;
-
-    let ps = PortSettings {
-        baud_rate: BaudRate::Baud115200,
-        char_size: CharSize::Bits8,
-        parity: Parity::ParityNone,
-        stop_bits: StopBits::Stop1,
-        flow_control: FlowControl::FlowSoftware,
-    };
-    port.configure(&ps)?;
-
-    let PIN_LED: u8 = 26;
-
-    let onmsg = Msgbuf {
-        payload: Payload {
-            payload_type: am::PayloadType::PtWritepin as u8, // .to_u8(),
-            data: PayloadData {
-                pinval: Pinval {
-                    pin: PIN_LED,
-                    state: 1,
-                },
-            },
-        },
-    };
-    let offmsg = Msgbuf {
-        payload: Payload {
-            payload_type: am::PayloadType::PtWritepin as u8,
-            data: PayloadData {
-                pinval: Pinval {
-                    pin: PIN_LED,
-                    state: 0,
-                },
-            },
-        },
-    };
-
-    unsafe {
-        println!("payload_type: {}", onmsg.payload.payload_type);
-    }
-
-    let mut onfile = File::create("onmsg.bin")?;
-    unsafe {
-        onfile.write(&onmsg.buf[0..6]);
-    }
-
-    let id: u8 = 1;
-
-    let mut on = true;
-
-    let mut retmsg = offmsg.clone();
-
-    loop {
-        unsafe {
-            if on {
-                writeMessage(&mut port, &onmsg, id)?;
-            } else {
-                writeMessage(&mut port, &offmsg, id)?;
-            }
-
-            let mut fromid: u8 = 0;
-
-            readMessage(&mut port, &mut retmsg, &mut fromid);
-        }
-
-        on = !on;
-
-        sleep(Duration::from_millis(20));
-    }
 }
 
 unsafe fn writeMessage(
