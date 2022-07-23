42adb5339d1203b72f3f3fd81b30da89ecd68b87
Ben Burdette <bburdette@protonmail.com>
2022-07-22 20:53:44 -0600

    attempting to write to serial

--- matoserver/server/src/interfaces.rs
+++ matoserver/server/src/interfaces.rs
@@ -3,8 +3,11 @@ use crate::config::Config;
 use crate::messages::{PublicMessage, ServerResponse};
 // use crate::sqldata;
 use crate::data::{AutomatoMsg, ListAutomato, ServerData};
+use automato::automatomsg as am;
 use log::info;
 use std::error::Error;
+use std::thread::sleep;
+use std::time::Duration;
 
 // public json msgs don't require login.
 pub fn public_interface(
@@ -20,7 +23,37 @@ pub fn public_interface(
         "AutomatoMsg" => {
             let msgdata = Option::ok_or(msg.data.as_ref(), "malformed json data")?;
             let am: AutomatoMsg = serde_json::from_value(msgdata.clone())?;
+            let mut mb = am::Msgbuf {
+                buf: [0; am::RH_RF95_MAX_MESSAGE_LEN],
+            };
 
+            let mut retmsg = mb.clone();
+
+            unsafe {
+                mb.payload = am::Payload::from(am.message);
+                let port = data.port.lock()?;
+                am::write_message(&mut port, &mb, am.id)?;
+
+                let mut fromid: u8 = 0;
+                sleep(Duration::from_millis(420));
+
+                match am::read_message(&mut port, &mut retmsg, &mut fromid) {
+                    Ok(true) => {
+                        println!("reply from: {}", fromid);
+                        // for i in 0..retmsg.buf.len() {
+                        //     let c = retmsg.buf[i];
+                        //     println!("{} - {}", c, c as char);
+                        // }
+                        am::print_payload(&retmsg.payload);
+                    }
+                    Ok(false) => {
+                        println!("here");
+                    }
+                    Err(e) => {
+                        println!("error: {:?}", e);
+                    }
+                }
+            }
             Ok(ServerResponse {
                 what: "unimplemented".to_string(),
                 content: serde_json::Value::Null,
--- rustlib/src/automatomsg.rs
+++ rustlib/src/automatomsg.rs
@@ -3,8 +3,9 @@ use num_derive::{FromPrimitive, ToPrimitive};
 use serde::de::Deserializer;
 use serde::ser::{SerializeSeq, Serializer};
 use serde::{Deserialize, Serialize};
+use serial;
+use std::io::{Read, Write};
 use std::mem::size_of;
-
 // --------------------------------------------------------
 // message structs.
 // --------------------------------------------------------
@@ -744,4 +745,44 @@ pub unsafe fn print_payload(p: &Payload) {
             println!("");
         }
     }
+}
+
+pub unsafe fn write_message(
+    port: &mut serial::SystemPort,
+    msg: &Msgbuf,
+    toid: u8,
+) -> Result<(), serial::Error> {
+    let sz = payload_size(&msg.payload);
+
+    port.write(&['m' as u8])?;
+    port.write(&[toid as u8])?;
+    port.write(&[sz as u8])?;
+    port.write(&msg.buf[0..sz + 1])?;
+
+    Ok(())
+}
+
+pub unsafe fn read_message(
+    port: &mut serial::SystemPort,
+    msg: &mut Msgbuf,
+    fromid: &mut u8,
+) -> Result<bool, serial::Error> {
+    let mut monobuf = [0; 1];
+
+    port.read_exact(&mut monobuf)?;
+    if monobuf[0] as char != 'm' {
+        return Ok(false);
+    }
+
+    port.read_exact(&mut monobuf)?;
+    *fromid = monobuf[0];
+
+    port.read_exact(&mut monobuf)?;
+    let sz = monobuf[0] as usize;
+
+    if sz > 0 {
+        port.read_exact(&mut msg.buf[0..sz])?;
+    }
+
+    Ok(true)
 }
