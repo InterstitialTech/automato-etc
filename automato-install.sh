cd7c337c0af2950ce0c431bfc68b84be6c2ce1b8
Ben Burdette <>
2025-12-04 14:46:30 -0700

    bump arduino-cli version

--- automato-install.sh
+++ automato-install.sh
@@ -1,5 +1,5 @@
 
-arduino-cli core install esp32:esp32@1.0.6 --additional-urls https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json
+arduino-cli core install esp32:esp32@3.3.2 --additional-urls https://raw.githubusercontent.com/espressif/arduino-esp32/gh-pages/package_esp32_index.json
 
 arduino-cli core install automato:esp32 --additional-urls https://raw.githubusercontent.com/InterstitialTech/automato-arduino/refs/heads/main/package_automato_index.json
 
